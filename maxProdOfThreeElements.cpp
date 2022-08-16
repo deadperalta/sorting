95d8ce63a2381e34549d9643a1d51fc106a863b8
deadperalta <86057977+deadperalta@users.noreply.github.com>
2022-08-16 15:02:37 +0530

    Create maxProdOfThreeElements.cpp

---
+++ maxProdOfThreeElements.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+//Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    long long maxProduct(int arr[], int n) {
+        sort(arr,arr+n);
+        
+        long long a1=arr[0];
+        long long a2=arr[1];
+        
+        long long b=arr[n-3], c=arr[n-2], d=arr[n-1];
+        
+        return max(a1*a2*d , b*c*d);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
